03b19e9fdc55fc0f6fa7b2c45c43e434d8123a47
bruninjaman <brubaco@hotmail.com>
2022-01-20 08:15:12 -0300

    Tradução e posição

--- index.php
+++ index.php
@@ -554,8 +554,8 @@
 	<div class="cd-user-modal"> <!-- this is the entire modal form, including the background -->
 		<div class="cd-user-modal-container"> <!-- this is the container wrapper -->
 			<ul class="cd-switcher">
-				<li><a href="#0">Sign in</a></li>
-				<li><a href="#0">New account</a></li>
+				<li><a href="#0">Entrar</a></li>
+				<li><a href="#0">Nova Conta</a></li>
 			</ul>
 
 			<div id="cd-login"> <!-- log in form -->
@@ -567,15 +567,15 @@
 					</p>
 
 					<p class="fieldset">
-						<label class="image-replace cd-password" for="signin-password">Password</label>
-						<input class="full-width has-padding has-border" id="signin-password" type="text"  placeholder="Password">
-						<a href="#0" class="hide-password">Hide</a>
+						<label class="image-replace cd-password" for="signin-password">Senha</label>
+						<input class="full-width has-padding has-border" id="signin-password" type="text"  placeholder="Senha">
+						<a href="#0" class="hide-password">Esconder</a>
 						<span class="cd-error-message">Error message here!</span>
 					</p>
 
 					<p class="fieldset">
 						<input type="checkbox" id="remember-me" checked>
-						<label for="remember-me">Remember me</label>
+						<label for="remember-me">Manter logado</label>
 					</p>
 
 					<p class="fieldset">
@@ -583,15 +583,15 @@
 					</p>
 				</form>
 				
-				<p class="cd-form-bottom-message"><a href="#0">Forgot your password?</a></p>
+				<p class="cd-form-bottom-message"><a href="#0">Esqueceu sua Senha?</a></p>
 				<!-- <a href="#0" class="cd-close-form">Close</a> -->
 			</div> <!-- cd-login -->
 
 			<div id="cd-signup"> <!-- sign up form -->
 				<form class="cd-form">
 					<p class="fieldset">
-						<label class="image-replace cd-username" for="signup-username">Username</label>
-						<input class="full-width has-padding has-border" id="signup-username" type="text" placeholder="Username">
+						<label class="image-replace cd-username" for="signup-username">Nome de Usuario</label>
+						<input class="full-width has-padding has-border" id="signup-username" type="text" placeholder="Nome de Usuario">
 						<span class="cd-error-message">Error message here!</span>
 					</p>
 
@@ -602,19 +602,19 @@
 					</p>
 
 					<p class="fieldset">
-						<label class="image-replace cd-password" for="signup-password">Password</label>
-						<input class="full-width has-padding has-border" id="signup-password" type="text"  placeholder="Password">
-						<a href="#0" class="hide-password">Hide</a>
+						<label class="image-replace cd-password" for="signup-password">Senha</label>
+						<input class="full-width has-padding has-border" id="signup-password" type="text"  placeholder="Senha">
+						<a href="#0" class="hide-password">Esconder</a>
 						<span class="cd-error-message">Error message here!</span>
 					</p>
 
 					<p class="fieldset">
 						<input type="checkbox" id="accept-terms">
-						<label for="accept-terms">I agree to the <a href="#0">Terms</a></label>
+						<label for="accept-terms">Eu concordo com os <a href="#0">termos de uso</a></label>
 					</p>
 
 					<p class="fieldset">
-						<input class="full-width has-padding" type="submit" value="Create account">
+						<input class="full-width has-padding" type="submit" value="Criar Conta">
 					</p>
 				</form>
 
@@ -622,7 +622,7 @@
 			</div> <!-- cd-signup -->
 
 			<div id="cd-reset-password"> <!-- reset password form -->
-				<p class="cd-form-message">Lost your password? Please enter your email address. You will receive a link to create a new password.</p>
+				<p class="cd-form-message">Esqueceu sua senha? Por favor digite seu endereço de email. Você receberá o link para mudar de senha.</p>
 
 				<form class="cd-form">
 					<p class="fieldset">
@@ -636,9 +636,9 @@
 					</p>
 				</form>
 
-				<p class="cd-form-bottom-message"><a href="#0">Back to log-in</a></p>
+				<p class="cd-form-bottom-message"><a href="#0">Volto para o login</a></p>
 			</div> <!-- cd-reset-password -->
-			<a href="#0" class="cd-close-form">Close</a>
+			<a href="#0" class="cd-close-form">Fechar</a>
 		</div> <!-- cd-user-modal-container -->
 	</div> <!-- cd-user-modal -->
 </body>
