2e607e4a37b1455b28430ab2e32f83339634b7c2
bruninjaman <brubaco@hotmail.com>
2022-05-23 15:21:13 -0300

    #tablesort2

--- tabelaPecas.php
+++ tabelaPecas.php
@@ -86,7 +86,7 @@ deletePeca($conn)
                   </div>
                </div>
             </div>
-            <table class="table table-striped table-hover">
+            <table class="table table-striped table-hover table-sortable">
                <thead>
                   <tr>
                      <th>
@@ -124,6 +124,7 @@ deletePeca($conn)
       <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>
       <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
       <script src="./assets/js/tabela_pecas.js"></script>
+      <script src="assets/js/table_sort.js"></script>
    </section>
 
    <!-- Footer -->
--- tabelaServicos.php
+++ tabelaServicos.php
@@ -85,7 +85,7 @@ deleteServico($conn);
                   </div>
                </div>
             </div>
-            <table class="table table-striped table-hover">
+            <table class="table table-striped table-hover table-sortable">
                <thead>
                   <tr>
                      <th>
@@ -121,6 +121,7 @@ deleteServico($conn);
       <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>
       <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
       <script src="./assets/js/tabela_servicos.js"></script>
+      <script src="assets/js/table_sort.js"></script>
    </section>
 
    <!-- Footer -->
