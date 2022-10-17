2c6169e031176071add4c97d8fe931488bfb28b8
bruninjaman <brubaco@hotmail.com>
2022-10-17 03:49:25 -0300

    tabela de serviços quase pronta

--- assets/php/includes/pages/services/sections/tabela.php
+++ assets/php/includes/pages/services/sections/tabela.php
@@ -2,49 +2,47 @@
     <div class="content">
         <center>
             <form method="post" action="assets/php/scripts/tabelaMotos/add-moto.php">
-                <h2>Pagina de ordem nº <?php echo $_GET['ordem']?></h2>
+                <h2>Pagina de ordem nº <?php echo $_GET['ordem'] ?></h2>
         </center>
 
         <div class="table-wrapper">
             <div class="table-wrapper">
+                <?php
+                $page = 5;
+                $items_ordem_query = 'SELECT 1 as type, pecas.grupo grupo, pecas.parte parte, pecas.item item, pecas.foto foto, pecas.valor, pecas.quantidade FROM pecas ';
+                $items_ordem_query .= "WHERE pecas.ordem = '". $_GET['ordem']."' ";
+                $items_ordem_query .= "UNION ";
+                $items_ordem_query .= "SELECT 2, Null, Null, servicos.item, Null, servicos.valor, 1 FROM servicos ";
+                $items_ordem_query .= "WHERE servicos.ordem = '". $_GET['ordem']."' ";
+                $items_ordem_query .= "UNION ";
+                $items_ordem_query .= "SELECT 3, null, null, adiantamento.descricao, Null, adiantamento.valor, 1 FROM adiantamento ";
+                $items_ordem_query .= "WHERE adiantamento.ordem = '". $_GET['ordem']."' ";
+                $items_ordem_query .= "LIMIT " . $page * 5;
+                $result = mysqli_query($conn, $items_ordem_query);
+                ?>
                 <table class="table">
                     <thead>
                         <tr>
                             <th>Foto</th>
-                            <th>Endereço</th>
-                            <th>Ano</th>
-                            <th>Modelo</th>
-                            <th>Marca</th>
-                            <th>Placa</th>
-                            <th>KM</th>
-                            <th>Proprietario</th>
-                            <th>Actions</th>
+                            <th colspan=3>Grupo/Parte/Item</th>
+                            <th>Quantidade</th>
+                            <th>Valor</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
-                        $page = 1;
-                        $sql_query = "SELECT * FROM motocicletas ";
-                        $sql_query .= "LIMIT " . $page * 5;
-                        $result = mysqli_query($conn, $sql_query);
 
-                        while ($moto = mysqli_fetch_assoc($result)) {
+                        while ($item = mysqli_fetch_assoc($result)) {
                         ?>
                             <tr>
-                                <td><img src='<?php echo $moto['foto']; ?>' style="height:100px;width:100px;"></td>
-                                <td><?php echo $moto['endereco']; ?></td>
-                                <td><?php echo $moto['ano']; ?></td>
-                                <td><?php echo $moto['modelo']; ?></td>
-                                <td><?php echo $moto['marca']; ?></td>
-                                <td><?php echo $moto['placa']; ?></td>
-                                <td><?php echo $moto['km']; ?></td>
-                                <td><?php echo $moto['proprietario']; ?></td>
+                                <td><img src='<?php echo $item['foto']; ?>' style="height:100px;width:100px;"></td>
+                                <td colspan=3><?php echo $item['grupo'] != null ? "".$item['grupo'] : ""; ?><?php echo $item['parte'] != null ? "/".$item['parte']. "/" : ""; ?><?php echo $item['item'] != null ? "".$item['item'] : ""; ?></td>
+                                <td><?php echo $item['quantidade']; ?></td>
+                                <td><?php echo $item['valor']; ?></td>
                                 <td>
-                                    <button onclick="location.href='tabelaMotoServices.php?motoID=<?php echo $moto['motoId'] ?>'"><i class="fa-solid fa-toolbox me-2"></i></i> Serviços</button>
-                                    <button onclick="location.href='tabelaMotoProprietario.php?motoID=<?php echo $moto['motoId'] ?>'"><i class="fa-solid fa-address-book me-2"></i> Proprietarios</button>
-                                    <button onclick="location.href='tabelaMotoAdd.php?motoID=<?php echo $moto['motoId'] ?>'"><i class="fa-solid fa-plus me-2"></i> Adicionar </button>
-                                    <button onclick="location.href='tabelaMotoEdit.php?motoID=<?php echo $moto['motoId'] ?>'"><i class="fa-solid fa-user-pen me-2"></i> Editar </button>
-                                    <button onclick="location.href='assets/php/scripts/tabelaMotos/delete-moto.php?motoID=<?php echo $moto['motoId'] ?>'" ><i class="fa-sharp fa-solid fa-delete-left me-2"></i> Deletar</button>
+                                    <button onclick="location.href='#'"><i class="fa-solid fa-plus me-2"></i> Adicionar </button>
+                                    <button onclick="location.href='#'"><i class="fa-solid fa-user-pen me-2"></i> Editar </button>
+                                    <button onclick="location.href='#'"><i class="fa-sharp fa-solid fa-delete-left me-2"></i> Deletar</button>
                                     <!-- <button onclick="return confirm('Deseja realmente excluir este item?')" ><i class="fa-sharp fa-solid fa-delete-left me-2"></i> Deletar</button> -->
                                 </td>
                             </tr>
