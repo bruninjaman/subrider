4620ddba9ccea2d4c6757a62be54e95dd3fb4442
bruninjaman <brubaco@hotmail.com>
2022-10-27 08:38:35 -0300

    pagination tabelaServicos

--- pages/tabelaServicos/sections/tabela.php
+++ pages/tabelaServicos/sections/tabela.php
@@ -14,9 +14,8 @@
                     </thead>
                     <tbody>
                         <?php
-                        $page = 1;
                         $sql_query = "SELECT * FROM servicos ";
-                        $sql_query .= "LIMIT " . $page * 5;
+                        $sql_query .= "LIMIT " . ((isset($_GET['page']) ? $_GET['page'] : 0) * 5) . ", 5";
                         $result = mysqli_query($conn, $sql_query);
 
                         while ($servico = mysqli_fetch_assoc($result)) {
@@ -27,7 +26,7 @@
                                 <td><?php echo $servico['tipo']; ?></td>
                                 <td><?php echo $servico['valor']; ?></td>
                                 <td>
-                                    <button onclick="location.href='tabelaServicosAdd.php?motoID=<?php echo $moto['motoId']?>'" ><i class="fa-solid fa-plus me-2"></i> Adicionar </button>
+                                    <button onclick="location.href='tabelaServicosAdd.php?servicoID=<?php echo $moto['servicoId']?>'" ><i class="fa-solid fa-plus me-2"></i> Adicionar </button>
                                     <button onclick="location.href='tabelaServicosEdit.php?servicoID=<?php echo $servico['servicoId']?>'" ><i class="fa-solid fa-user-pen me-2"></i> Editar </button>
                                     <button onclick="return delete_confirm('Deseja realmente excluir este item?',<?php echo $servico['servicoId'] ?>)" ><i class="fa-sharp fa-solid fa-delete-left me-2"></i> Deletar</button>
                                 </td>
@@ -37,16 +36,16 @@
                         ?>
                     </tbody>
                 </table>
-                <div class="w3-twothird">
+                <div class="pagination-style">
                     <ul class="pagination">
-                        <li><a href="javascript:void(0)">«</a></li>
-                        <li><a class="w3-red" href="javascript:void(0)">1</a></li>
-                        <li><a href="javascript:void(0)">2</a></li>
-                        <li><a href="javascript:void(0)">3</a></li>
-                        <li><a href="javascript:void(0)">4</a></li>
-                        <li><a href="javascript:void(0)">5</a></li>
-                        <li><a href="javascript:void(0)">6</a></li>
-                        <li><a href="javascript:void(0)">»</a></li>
+                        <li><a href="tabelaServicos.php?page=<?php echo isset($_GET['page']) && $_GET['page'] > 0  ? $_GET['page']-1 : 0 ?>">«</a></li>
+                        <li><a href="tabelaServicos.php?page=<?php echo 1 ?>">1</a></li>
+                        <li><a href="tabelaServicos.php?page=<?php echo 2 ?>">2</a></li>
+                        <li><a href="tabelaServicos.php?page=<?php echo 3 ?>">3</a></li>
+                        <li><a href="tabelaServicos.php?page=<?php echo 4 ?>">4</a></li>
+                        <li><a href="tabelaServicos.php?page=<?php echo 5 ?>">5</a></li>
+                        <li><a href="tabelaServicos.php?page=<?php echo 6 ?>">6</a></li>
+                        <li><a href="tabelaServicos.php?page=<?php echo isset($_GET['page']) ? $_GET['page']+1 : 0 ?>">»</a></li>
                     </ul>
                 </div>
             </div>
