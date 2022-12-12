ff4401141a358f4de07e0c2ccbb1b3ecb3e85c90
bruninjaman <brubaco@hotmail.com>
2022-12-12 11:17:31 -0300

    edi8t-servico.php removido ordem e valor

--- scripts/tabelaServicosEdit/edit-servico.php
+++ scripts/tabelaServicosEdit/edit-servico.php
@@ -10,14 +10,12 @@ require_once("../functions.php");
 
 if (isset($_POST['item'])) {
     $item = $_POST['item'];
-    $ordem = $_POST['ordem'];
     $tipo = $_POST['tipo'];
-    $valor = $_POST['valor'];
     $servicoID= $_POST['servicoID'];
 
     $mysqli_query = " UPDATE servicos "; //UPDATE
     $mysqli_query .= " SET item = '{$item}', ";
-    $mysqli_query .= " ordem = '{$ordem}', tipo = '{$tipo}', valor = '{$valor}' ";
+    $mysqli_query .= " tipo = '{$tipo}' ";
     $mysqli_query .= " WHERE servicoId = '" . $servicoID . "'";
 
     mysqli_query($conn, $mysqli_query);
