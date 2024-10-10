e0b442168d0ba1433d0d8e5c16cc17eb62909cef
Bruno Coelho <brubaco@hotmail.com>
2024-10-10 15:18:35 -0300

    menu embreagem feito

--- pages/ordemservico/menu_embreagem.php
+++ pages/ordemservico/menu_embreagem.php
@@ -1,2 +1,39 @@
-<h2>Informações da Embreagem</h2>
-<a class="button secondary" id="backToMenu">Voltar ao Menu</a>
+<h1>Menu Embreagem</h1>
+<form action="#" method="POST">
+    <div class="row">
+        <div class="col-4">
+            <label>Número de Discos:</label>
+        </div>
+        <div class="col-3">
+            <input type="number" name="nr_discos" step="1" required><br><br>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <label>Número de Discos Separadores:</label>
+        </div>
+        <div class="col-3">
+            <input type="number" name="nr_discos_sep" step="1" required><br><br>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <label>Disco Fricção ESP MIN: </label>
+        </div>
+        <div class="col-3">
+            <input type="number" name="disco_fric_esp_min" step="0.1" required><br><br>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <label>Disco SEP EMPEN MAX:</label>
+        </div>
+        <div class="col-3">
+            <input type="number" name="disco_sep_emp_max" step="0.1" required><br><br>
+        </div>
+    </div>
+
+    <!-- Botão de envio -->
+    <button type="submit" class="button primary">Salvar</button>
+    <a class="button secondary" id="backToMenu">Voltar ao Menu</a>
+</form>
