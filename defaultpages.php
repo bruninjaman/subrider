95e55121dc02f231c6e571cd951621dbe90da02e
Bruno Coelho <brubaco@hotmail.com>
2025-03-23 22:01:21 -0300

    Atualiza o título da página para "Sub-rider"

--- defaultpages.php
+++ defaultpages.php
@@ -18,7 +18,7 @@
 <html>
 
 <head>
-    <title>Subrider</title>
+    <title>Sub-rider</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="assets/css/main.css" />
