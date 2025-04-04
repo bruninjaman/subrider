783976a72381b125170f79c396182e9cccb34d12
Bruno Coelho <brubaco@hotmail.com>
2025-04-04 12:13:50 -0300

    Session should last 30 days now

--- scripts/log-in.php
+++ scripts/log-in.php
@@ -4,6 +4,11 @@ require_once("../connection/connection.php");
 //FUNCTIONS
 require_once("functions.php");
 
+// Configuração da sessão para 30 dias
+ini_set('session.gc_maxlifetime', 30 * 24 * 60 * 60); // 30 dias em segundos
+ini_set('session.cookie_lifetime', 30 * 24 * 60 * 60); // 30 dias em segundos
+session_set_cookie_params(30 * 24 * 60 * 60); // 30 dias em segundos
+
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Validate that required fields exist
