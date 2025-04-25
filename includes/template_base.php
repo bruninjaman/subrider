13dff105bedc824dfaf0c024f7e4610869e42261
Bruno Coelho <brubaco@hotmail.com>
2025-04-25 00:32:46 -0300

    removed $baseadress and fixed paths

--- includes/template_base.php
+++ includes/template_base.php
@@ -6,11 +6,11 @@ require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config.php');
 session_start();
 
 //PERM
-require_once(PROJECT_ROOT_PATH . DS . "scripts" . DS . "perm.php");
+require_once("./scripts/perm.php");
 //CONNECTION
-require_once(PROJECT_ROOT_PATH . DS . "connection" . DS . "connection.php");
+require_once("./connection/connection.php");
 //FUNCTIONS
-require_once(PROJECT_ROOT_PATH . DS . "scripts" . DS . "functions.php");
+require_once("./scripts/functions.php");
 ?>
 <!--
 	Landed by HTML5 UP
@@ -23,13 +23,13 @@ require_once(PROJECT_ROOT_PATH . DS . "scripts" . DS . "functions.php");
     <title>Subrider</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <link rel="stylesheet" href="<?php echo PROJECT_ROOT_URL; ?>/assets/css/main.css" />
-    <link rel="stylesheet" href="<?php echo PROJECT_ROOT_URL; ?>/assets/css/search.css" />
-    <link rel="stylesheet" href="<?php echo PROJECT_ROOT_URL; ?>/assets/css/sort.css" />
-    <link rel="stylesheet" href="<?php echo PROJECT_ROOT_URL; ?>/assets/css/table.css" />
-    <link rel="stylesheet" href="<?php echo PROJECT_ROOT_URL; ?>/assets/css/fontawesome6.all.min.css" />  
+    <link rel="stylesheet" href="./assets/css/main.css" />
+    <link rel="stylesheet" href="./assets/css/search.css" />
+    <link rel="stylesheet" href="./assets/css/sort.css" />
+    <link rel="stylesheet" href="./assets/css/table.css" />
+    <link rel="stylesheet" href="./assets/css/fontawesome6.all.min.css" />  
     <noscript>
-        <link rel="stylesheet" href="<?php echo PROJECT_ROOT_URL; ?>/assets/css/noscript.css" />
+        <link rel="stylesheet" href="./assets/css/noscript.css" />
     </noscript>
 </head>
 
@@ -43,15 +43,15 @@ require_once(PROJECT_ROOT_PATH . DS . "scripts" . DS . "functions.php");
         ?>
     </div>
     <!-- Scripts for main theme -->
-    <script src="<?php echo PROJECT_ROOT_URL; ?>/assets/js/global/jquery.min.js"></script>
-    <script src="<?php echo PROJECT_ROOT_URL; ?>/assets/js/global/jquery.scrolly.min.js"></script>
-    <script src="<?php echo PROJECT_ROOT_URL; ?>/assets/js/global/jquery.dropotron.min.js"></script>
-    <script src="<?php echo PROJECT_ROOT_URL; ?>/assets/js/global/jquery.scrollex.min.js"></script>
-    <script src="<?php echo PROJECT_ROOT_URL; ?>/assets/js/global/browser.min.js"></script>
-    <script src="<?php echo PROJECT_ROOT_URL; ?>/assets/js/global/breakpoints.min.js"></script>
-    <script src="<?php echo PROJECT_ROOT_URL; ?>/assets/js/global/util.js"></script>
-    <script src="<?php echo PROJECT_ROOT_URL; ?>/assets/js/main.js"></script>
-    <script src="<?php echo PROJECT_ROOT_URL; ?>/assets/js/delete_confirm.js"></script>
+    <script src="./assets/js/global/jquery.min.js"></script>
+    <script src="./assets/js/global/jquery.scrolly.min.js"></script>
+    <script src="./assets/js/global/jquery.dropotron.min.js"></script>
+    <script src="./assets/js/global/jquery.scrollex.min.js"></script>
+    <script src="./assets/js/global/browser.min.js"></script>
+    <script src="./assets/js/global/breakpoints.min.js"></script>
+    <script src="./assets/js/global/util.js"></script>
+    <script src="./assets/js/main.js"></script>
+    <script src="./assets/js/delete_confirm.js"></script>
 </body>
 
 </html>
