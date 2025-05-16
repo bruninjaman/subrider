7e22f00ed3c4f03b31674a91f232d29fd46c0da0
Bruno Coelho <brubaco@hotmail.com>
2025-05-16 20:06:07 -0300

    PDF funciona na versão local

--- pdf/download.php
+++ pdf/download.php
@@ -10,16 +10,57 @@ require_once("./../connection/connection.php");
 //FUNCTIONS
 require_once("./../scripts/functions.php");
 
+// Incluir o script de busca de imagens
+require_once(__DIR__ . '/../scripts/peca_imagem.php');
+
 $options = new Options();
 $options->setChroot(__DIR__);
 $options->setIsRemoteEnabled(true);
 
 $dompdf = new Dompdf($options);
 
-//Pegar todos items pertencentes a esta ordem de serviço
-$items_ordem_query = "SELECT * FROM item_ordem ";
-$items_ordem_query .= "WHERE item_ordem.Ordem = '" . $_GET['ordem'] . "' ";
+// Consulta todos os itens da ordem
+$items_ordem_query = "SELECT * FROM item_ordem WHERE item_ordem.Ordem = '" . $_GET['ordem'] . "' ";
 $result = mysqli_query($conn, $items_ordem_query);
+
+// Separar itens por categoria
+$pecas = [];
+$servicos = [];
+$adiantamentos = [];
+
+while ($item = mysqli_fetch_assoc($result)) {
+    if ($item['Categoria'] == '3') {
+        $adiantamentos[] = $item;
+    } 
+    // Peças (categoria 2)
+    else if ($item['Categoria'] == '2') {
+        $pecas[] = $item;
+    } 
+    // Serviços (categoria 1)
+    else {
+        $servicos[] = $item;
+    }
+}
+
+// Cálculos de totais
+$total_pecas = 0;
+$total_servicos = 0;
+$total_adiantamentos = 0;
+
+foreach ($pecas as $item) {
+    $total_pecas += $item['Valor'] * $item['Quantidade'];
+}
+
+foreach ($servicos as $item) {
+    $total_servicos += $item['Valor'] * $item['Quantidade'];
+}
+
+foreach ($adiantamentos as $item) {
+    $total_adiantamentos += $item['Valor'] * $item['Quantidade'];
+}
+
+$total_geral = $total_pecas + $total_servicos;
+$saldo = $total_geral - $total_adiantamentos;
 
 //Pegar informações da moto vinculada a esta ordem de serviço
 $motoinfo_query = "SELECT * FROM motocicletas ";
@@ -46,7 +87,7 @@ while ($ordeminfo = mysqli_fetch_assoc($result3)) {
 //Pegar informações sobre a moto
 while ($motoinfo = mysqli_fetch_assoc($result2)) {
   //$nome = $motoinfo['proprietario'];
-  $fone = "61 91111-1111";
+  $fone = $motoinfo['contato'] ?? "61 91111-1111";
   $endereco = $motoinfo['endereco'];
   $marca = $motoinfo['marca'];
   $placa = $motoinfo['placa'];
@@ -54,21 +95,25 @@ while ($motoinfo = mysqli_fetch_assoc($result2)) {
   $ano = $motoinfo['ano'];
 }
 
-//Inicializar total e adiantamento em 0
-$total = 0;
-$adiantamento = 0;
-
-
 //HTML antes de adicionar items
 $loadhtmlstring = '<!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Arquivo PDF</title>
+    <title>Ordem de Serviço - ' . $_GET['ordem'] . '</title>
 
     <style>
+      * {
+        box-sizing: border-box;
+      }
+      body {
+        font-family: Arial, sans-serif;
+        line-height: 1.3;
+        color: #333;
+        font-size: 10px;
+      }
       .total {
         font-size: 12px;
       }
@@ -77,7 +122,17 @@ $loadhtmlstring = '<!DOCTYPE html>
       }
       .valores-totais {
         color: black;
-        font-style: bold;
+        font-weight: bold;
+      }
+      .section-title {
+        background-color: #0a1e58;
+        color: white;
+        font-weight: bold;
+        padding: 5px;
+        font-size: 12px;
+        margin-top: 15px;
+        margin-bottom: 5px;
+        border-radius: 3px;
       }
       #ordem {
         text-align: right;
@@ -110,8 +165,7 @@ $loadhtmlstring = '<!DOCTYPE html>
         height: 55px;
         position: absolute;
       }
-      th,
-      td {
+      th, td {
         padding: 1.3px;
       }
       table {
@@ -121,30 +175,53 @@ $loadhtmlstring = '<!DOCTYPE html>
         font-size: 10px;
         background-color: rgb(218, 218, 218);
         border-radius: 5px;
+        margin-bottom: 5px;
       }
       table thead {
         background-color: #dadde6;
         font-size: 11px;
         color: rgb(3, 3, 3);
-        font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+        font-family: Arial, sans-serif;
       }
-      table,
-      th,
-      td {
+      table, th, td {
         border: 0.1px solid white;
       }
       table tbody {
         background-color: #eee;
         text-align: center;
-        font-family: "ptsans", serif;
+        font-family: Arial, sans-serif;
       }
-      /*  */
       table tbody tr td {
         padding: 1px;
         color: #1f274d;
       }
       tr:last-child {
         background-color: #eee;
+      }
+      .item-image {
+        width: 30px;
+        height: 30px;
+        object-fit: cover;
+        border-radius: 3px;
+        margin-right: 5px;
+        vertical-align: middle;
+      }
+      .item-description {
+        display: flex;
+        align-items: center;
+        text-align: left;
+      }
+      .resumo-table td {
+        text-align: left;
+        padding: 5px;
+      }
+      .resumo-table tr:last-child {
+        background-color: #dadde6;
+        font-weight: bold;
+        font-size: 12px;
+      }
+      .resumo-table tr:last-child td:last-child {
+        color: #e44c65;
       }
     </style>
   </head>
@@ -169,7 +246,7 @@ $loadhtmlstring .= '</a>
 <th>Data:</th>
 <th class="head-content" colspan="3">';
 
-$loadhtmlstring .=  $data;
+$loadhtmlstring .=  date("d/m/Y", strtotime($data));
 
 $loadhtmlstring .= '<th>Km:</th>
 <th class="head-content">';
@@ -228,127 +305,196 @@ $loadhtmlstring .= $ano;
 
 $loadhtmlstring .= '</th>
     </tr>
+  </thead>
+</table>';
+
+// Adicionar tabela de serviços
+$loadhtmlstring .= '<div class="section-title">Serviços</div>
+<table class="table">
+  <thead>
     <tr>
-      <th class="main-th">Ordem</th>
-      <th class="main-th" colspan="2">Descrição</th>
-      <th class="main-th">Quantidade</th>
-      <th class="main-th">Valor unitário</th>
-      <th class="main-th">Valor Total</th>
+      <th class="main-th" width="45%">Descrição</th>
+      <th class="main-th" width="15%">Quantidade</th>
+      <th class="main-th" width="20%">Valor unitário</th>
+      <th class="main-th" width="20%">Valor Total</th>
     </tr>
   </thead>
   <tbody>';
 
-
-
-
-$total;
-$adiantamento;
-$itemcount = 0;
-
-
-while ($item = mysqli_fetch_assoc($result)) {
-  $itemcount++;
-  if ($item["Categoria"] != '3') {
-    $total = $total + $item['Valor'] * $item['Quantidade'];
-  } else {
-    $adiantamento = $adiantamento  + $item['Valor'] * $item['Quantidade'];
-  }
-  if ($item['Categoria'] == '3')
-    continue;
-
-  //item da tabela
-  $loadhtmlstring .= '<tr>
-    <td>'. $itemcount . '</td>
-    <td colspan="2">';
-
-
-  $loadhtmlstring .= $item['Tipo'] != '0' ? "" . $item['Tipo'] . " - " : "";
-  $loadhtmlstring .= $item['Grupo'] != '0' ? "" . $item['Grupo'] . " - " : "";
-  $loadhtmlstring .= $item['Item'] != '0' ? "" . $item['Item'] . "" : "";
-  $loadhtmlstring .= $item['Parte'] != '0' ? " / " . $item['Parte'] : "";
-  $loadhtmlstring .= $item['Descricao'] != '0' ? "" . $item['Descricao'] : "";
-  if ($item['Categoria'] == '2')
-    $loadhtmlstring .= $item['Codigo'] != '0' ? "(" . $item['Codigo'] . ")" : "";
-
-  $loadhtmlstring .= '</td>
-    <td>';
-
-  $loadhtmlstring .= $item['Quantidade'];
-
-  $loadhtmlstring .= '</td>
-    <td>';
-
-  $loadhtmlstring .= ($item['Valor'] == 0) ? 'N/D' : realFormat($item['Valor']);
-
-  $loadhtmlstring .= '</td>
-    <td>';
-
-  $loadhtmlstring .= realFormat($item['Valor'] * $item['Quantidade']);
-
-  $loadhtmlstring .= '</td>
+if (count($servicos) > 0) {
+  foreach ($servicos as $item) {
+    $loadhtmlstring .= '<tr>
+      <td style="text-align: left;">';
+      
+    $loadhtmlstring .= $item['Tipo'] != '0' ? "" . $item['Tipo'] . " - " : "";
+    $loadhtmlstring .= $item['Grupo'] != '0' ? "" . $item['Grupo'] . " - " : "";
+    $loadhtmlstring .= $item['Item'] != '0' ? "" . $item['Item'] . "" : "";
+    $loadhtmlstring .= $item['Parte'] != '0' ? " / " . $item['Parte'] : "";
+    $loadhtmlstring .= $item['Descricao'] != '0' ? "" . $item['Descricao'] : "";
+      
+    $loadhtmlstring .= '</td>
+      <td>' . $item['Quantidade'] . '</td>
+      <td>' . (($item['Valor'] <= 0) ? 'N/D' : realFormat($item['Valor'])) . '</td>
+      <td>' . realFormat($item['Valor'] * $item['Quantidade']) . '</td>
     </tr>';
-  //
+  }
+} else {
+  $loadhtmlstring .= '<tr><td colspan="4" style="text-align: center;">Nenhum serviço adicionado</td></tr>';
 }
-$saldo = $total - $adiantamento;
 
 $loadhtmlstring .= '<tr class="total">
-<td colspan="4"></td>
-<td class="valores-totais">SubTotal:</td>
-<td class="valores-totais">';
-
-$loadhtmlstring .= realFormat($total);
-
-$loadhtmlstring .= '</td>
+  <td colspan="2"></td>
+  <td class="valores-totais">Total Serviços:</td>
+  <td class="valores-totais">'. realFormat($total_servicos) .'</td>
 </tr>
 </tbody>
-</table>
-<br />
+</table>';
+
+// Adicionar tabela de peças
+$loadhtmlstring .= '<div class="section-title">Peças</div>
 <table class="table">
-<tbody>';
+  <thead>
+    <tr>
+      <th class="main-th" width="45%">Descrição</th>
+      <th class="main-th" width="15%">Quantidade</th>
+      <th class="main-th" width="20%">Valor unitário</th>
+      <th class="main-th" width="20%">Valor Total</th>
+    </tr>
+  </thead>
+  <tbody>';
 
-$loadhtmlstring .= $total;
-
-$result = mysqli_query($conn, $items_ordem_query);
-while ($item = mysqli_fetch_assoc($result)) {
-  if ($item['Categoria'] != '3')
-    continue;
-  $loadhtmlstring .= '<tr>
-        <td class="adiantamento" colspan="2">';
-  $loadhtmlstring .= $item['Descricao'];
-  $loadhtmlstring .= '</td>
-        <td class="valores">';
-  $loadhtmlstring .= realFormat($item['Valor'] * $item['Quantidade']);
-  $loadhtmlstring .= '</td>
-        </tr>';
+if (count($pecas) > 0) {
+  foreach ($pecas as $item) {
+    // Buscar imagem da peça
+    $imagem_peca = '';
+    
+    // Primeiro, verificar se o item já tem um campo Foto
+    if ($item['Foto'] != '0' && !empty($item['Foto'])) {
+      // Usar a foto diretamente do item da ordem
+      $imagem_peca = 'https://www.subrider.com.br/' . $item['Foto'];
+    } 
+    // Se não tem foto direta, tentar buscar pelo código
+    else if ($item['Codigo'] != '0' && !empty($item['Codigo'])) {
+      // Usar a função que criamos para buscar a imagem
+      $imagem_temp = buscarImagemPeca($item['Codigo']);
+      $imagem_peca = str_replace($baseAddress, 'https://www.subrider.com.br', $imagem_temp);
+    } 
+    // Se nada funcionar, usar a padrão
+    else {
+      $imagem_peca = 'https://www.subrider.com.br/assets/css/images/peca-padrao.png';
+    }
+    
+    $loadhtmlstring .= '<tr>
+      <td style="text-align: left;">
+        <div class="item-description">
+          <img src="' . $imagem_peca . '" alt="Imagem da Peça" class="item-image" />
+          ';
+      
+    $loadhtmlstring .= $item['Tipo'] != '0' ? "" . $item['Tipo'] . " - " : "";
+    $loadhtmlstring .= $item['Grupo'] != '0' ? "" . $item['Grupo'] . " - " : "";
+    $loadhtmlstring .= $item['Item'] != '0' ? "" . $item['Item'] . "" : "";
+    $loadhtmlstring .= $item['Parte'] != '0' ? " / " . $item['Parte'] : "";
+    $loadhtmlstring .= $item['Descricao'] != '0' ? "" . $item['Descricao'] : "";
+    $loadhtmlstring .= $item['Codigo'] != '0' ? " (" . $item['Codigo'] . ")" : "";
+      
+    $loadhtmlstring .= '</div>
+      </td>
+      <td>' . $item['Quantidade'] . '</td>
+      <td>' . (($item['Valor'] <= 0) ? 'N/D' : realFormat($item['Valor'])) . '</td>
+      <td>' . realFormat($item['Valor'] * $item['Quantidade']) . '</td>
+    </tr>';
+  }
+} else {
+  $loadhtmlstring .= '<tr><td colspan="4" style="text-align: center;">Nenhuma peça adicionada</td></tr>';
 }
-$loadhtmlstring .= '<tr>
-<td class="saldo">Saldo:</td>
-<td class="valores-totais" colspan="2">';
 
-$loadhtmlstring .= realFormat($saldo);
-
-$loadhtmlstring .= '</td>
+$loadhtmlstring .= '<tr class="total">
+  <td colspan="2"></td>
+  <td class="valores-totais">Total Peças:</td>
+  <td class="valores-totais">'. realFormat($total_pecas) .'</td>
 </tr>
 </tbody>
-</table>
-</div>
+</table>';
+
+// Adicionar tabela de adiantamentos
+$loadhtmlstring .= '<div class="section-title">Adiantamentos (Pagamentos Recebidos)</div>
+<table class="table">
+  <thead>
+    <tr>
+      <th class="main-th" width="45%">Descrição</th>
+      <th class="main-th" width="15%">Quantidade</th>
+      <th class="main-th" width="20%">Valor unitário</th>
+      <th class="main-th" width="20%">Valor Total</th>
+    </tr>
+  </thead>
+  <tbody>';
+
+if (count($adiantamentos) > 0) {
+  foreach ($adiantamentos as $item) {
+    $loadhtmlstring .= '<tr>
+      <td style="text-align: left;">';
+      
+    $loadhtmlstring .= $item['Descricao'] != '0' ? "" . $item['Descricao'] : "Pagamento";
+      
+    $loadhtmlstring .= '</td>
+      <td>' . $item['Quantidade'] . '</td>
+      <td>' . (($item['Valor'] <= 0) ? 'N/D' : realFormat($item['Valor'])) . '</td>
+      <td>' . realFormat($item['Valor'] * $item['Quantidade']) . '</td>
+    </tr>';
+  }
+} else {
+  $loadhtmlstring .= '<tr><td colspan="4" style="text-align: center;">Nenhum adiantamento registrado</td></tr>';
+}
+
+$loadhtmlstring .= '<tr class="total">
+  <td colspan="2"></td>
+  <td class="valores-totais">Total Adiantamentos:</td>
+  <td class="valores-totais">'. realFormat($total_adiantamentos) .'</td>
+</tr>
+</tbody>
+</table>';
+
+// Adicionar resumo
+$loadhtmlstring .= '<div class="section-title">Resumo</div>
+<table class="table resumo-table">
+  <tbody>
+    <tr>
+      <td width="60%"><strong>Total de Serviços:</strong></td>
+      <td width="40%">'. realFormat($total_servicos) .'</td>
+    </tr>
+    <tr>
+      <td><strong>Total de Peças:</strong></td>
+      <td>'. realFormat($total_pecas) .'</td>
+    </tr>
+    <tr>
+      <td><strong>Valor Total:</strong></td>
+      <td>'. realFormat($total_geral) .'</td>
+    </tr>
+    <tr>
+      <td><strong>Total de Adiantamentos:</strong></td>
+      <td>'. realFormat($total_adiantamentos) .'</td>
+    </tr>
+    <tr>
+      <td><strong>Total a Pagar:</strong></td>
+      <td>'. realFormat($saldo) .'</td>
+    </tr>
+  </tbody>
+</table>';
+
+$loadhtmlstring .= '</div>
 </body>
 </html>';
 
-// var_dump(htmlspecialchars($loadhtmlstring));
-// die();
-
 $dompdf->loadHtml($loadhtmlstring);
 
-//testing new pdf
-//$dompdf->loadHtmlFile(__DIR__ . '/ordem.html');
+// Configurar tamanho do papel e orientação
+$dompdf->setPaper('A4', 'portrait');
 
-
+// Renderizar PDF
 $dompdf->render();
 
+// Enviar o PDF para o navegador
 header('content-type: application/pdf');
-
-//echo $dompdf->stream();
 echo $dompdf->output();
 
 mysqli_close($conn);
